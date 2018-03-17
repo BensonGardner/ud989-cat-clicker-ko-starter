98039d514d62def97ed16dfe482361f633535382
bensongardner <bensongardner@yahoo.com>
2018-03-17 18:36:58 -0500

    fix: Update file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var ViewModel = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Jinko');
-    this.imgsrc = ko.observable('img/22252709_010df3379e_z.jpg');
+    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
     this.imgAttribution = ko.observable('https://www.glickr.com/photos/');
     
     this.incrementCounter = function() {
