97450ceee558df1091f8695996a62ac28d5c561e
bensongardner <bensongardner@yahoo.com>
2018-03-17 19:16:43 -0500

    feat: Add cat levels

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 var ViewModel = function() {
     this.clickCount = ko.observable(0);
+    this.level = ko.computed(function() {
+        if (this.clickCount() < 10) {
+            return 'Infant';
+        } else if (this.clickCount() < 50) {
+            return 'Kitten';
+        } else {
+            return 'Established cat'; 
+        };
+    }, this);
     this.name = ko.observable('Jinko');
     this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
     this.imgAttribution = ko.observable('https://www.glickr.com/photos/');
     
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
+        console.log(this.clickCount());
     };
 }
 
